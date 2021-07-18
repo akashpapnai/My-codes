4d5e89d53d0cad5e639dd3770403f65f54a62eb5
akashpapnai <akashpapnai@gmail.com>
2021-07-18 12:10:28 +0530

    ib

--- leetcode/dp_max_number_of_points.cpp
+++ leetcode/dp_max_number_of_points.cpp
@@ -17,10 +17,21 @@ public:
         }
         
         for (int i = 1; i < points.size(); ++i) {
+            vector<long long> left_dp(points[i].size(), -1);
+            vector<long long> right_dp(points[i].size(), -1);
+            
+            left_dp[0] = dp[i - 1][0];
+            for (int k = 1; k < points[i].size(); ++k) {
+                left_dp[k] = max(left_dp[k - 1], dp[i - 1][k] + k);
+            }
+            
+            right_dp.back() = dp[i - 1].back() - points[i].size() + 1;
+            for (int k = points[i].size() - 2; k >= 0; --k) {
+                right_dp[k] = max(right_dp[k + 1], dp[i - 1][k] - k);
+            }
+            
             for (int j = 0; j < points[i].size(); ++j) {
-                for (int k = 0; k < points[i].size(); ++k) {
-                    dp[i][j] = max(dp[i][j], dp[i - 1][k] - abs(k - j) + points[i][j]);
-                }
+                dp[i][j] = max(left_dp[j] - j, right_dp[j] + j) + points[i][j];
             }
         }
         
